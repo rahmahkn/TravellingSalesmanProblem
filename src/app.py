09260b14352f67157b26abb27b3e98f7467e15b1
rahmahkn <rahmah.ks.nurdini@gmail.com>
2021-06-15 00:21:33 +0700

    Adding frontends

--- src/app.py
+++ src/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-from flask import render_template
+from flask import *
+from processing.tsp_solver import *
 
 app = Flask(__name__)
+global destNames, destCoors
+destNames, destCoors = [], []
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    global destNames, destCoors
+
+    # Getting data from form
+    courierName = request.form.get("courierName")
+    courierPace = request.form.get("courierPace")
+    originName = request.form.get("startName")
+    originCoorX = request.form.get("startCoorX")
+    originCoorY = request.form.get("startCoorY")
+    destName = request.form.get("destName")
+    destCoorX = request.form.get("destCoorX")
+    destCoorY = request.form.get("destCoorY")
+
+    if (destNames != [] or destCoors != []):
+        destNames += [destName]
+        destCoors += [Coordinate(float(destCoorX), float(destCoorY), destName)]
+
+    print(destNames)
+    print(destCoors)
+
+    return render_template('index.html', addedOrigin=destName)
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/processing/convert.py
@@ -0,0 +1,5 @@
+# Convert list to html table
+
+# Convert external file to Branch and Bound elements
+
+# Convert added origins to html style
