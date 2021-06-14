428584f8bd018220a0cf7a42438087743681b0f4
rahmahkn <rahmah.ks.nurdini@gmail.com>
2021-06-14 21:57:46 +0700

    Algorithm correctly finish

--- src/processing/tsp_solver.py
+++ src/processing/tsp_solver.py
@@ -11,25 +11,29 @@ class Coordinate():
     def eucDistance(self, otherCoor):
         return ((otherCoor.x-self.x)**2 + (otherCoor.y-self.y)**2)** 0.5
 
-class Edge():
-    def __init__(self, row, column):
-        self.row = row
-        self.column = column
-
-    # def getName(self):
-    #     return self.coor.name
-
 class Path():
-    def __init__(self, pathList):
-        self.path = pathList # list of Coordinates that make the path
+    def __init__(self, coordinateList, indexList):
+        self.coordinates = coordinateList # list of Coordinates that make the path
+        self.index = indexList # list of Index that correspond to coordinateList
 
     # Function to count distance of a path from first to last vertex 
     def countDistance(self):
         dist = 0
-        for i in range (len(self.path)-1):
-            dist += self.path[i].eucDistance(self.path[i]+1)
+        for i in range (len(self.index)-1):
+            dist += self.coordinates[self.index[i]].eucDistance(self.coordinates[self.index[i+1]])
         
         return dist
+
+    # Function to get path in string
+    def pathToString(self):
+        result = ''
+        for i in range (len(self.index)):
+            result += self.coordinates[self.index[i]].name
+
+            if (i != len(self.index)-1):
+                result += ("→")
+
+        return result
 
 class Graph():
     def __init__(self, coordinates):
@@ -40,7 +44,7 @@ class Graph():
         for i in range(self.nbVertex):
             for j in range(self.nbVertex):
                 if (i == j):
-                    self.distMatrix[i][j] = np.inf
+                    self.distMatrix[i][j] = float('inf')
 
     def reduceMatrix(self, matrix, row, col, start):
         sumReduce = 0
@@ -49,57 +53,46 @@ class Graph():
         # Making elements in "row" and "column" infinite
         # and element with index (col,row) infinite
         if (row != -1 or col != -1):
-            for j in range (len(matrix[0])):
-                reducedMatrix[row][j] = np.inf
+            for j in range (len(reducedMatrix[0])):
+                reducedMatrix[row][j] = float('inf')
 
-            for i in range (len(matrix[0])):
-                reducedMatrix[i][col] = np.inf
+            for i in range (len(reducedMatrix[0])):
+                reducedMatrix[i][col] = float('inf')
 
-            reducedMatrix[col][row] = np.inf
+            reducedMatrix[col][row] = float('inf')
 
         # Preventing back to start point before end
-        reducedMatrix[col][start] = np.inf
+        reducedMatrix[col][start] = float('inf')
 
         # Scanning rows, reducing row with its minimum element
-        for i in range (len(matrix[0])):
-            counter = 0
-            temp = 0
+        for i in range (len(reducedMatrix[0])):
+            # If the minimum is not 0 or infinite, substract all elements in the row
+            # with the minimum value
+            minValue = min(reducedMatrix[i])
+            if (minValue != 0 and minValue != float('inf')):
+                for j in range (self.nbVertex):
+                    reducedMatrix[i][j] -= minValue
 
-            for j in range (len(matrix[0])):
-                if (reducedMatrix[i][j] != np.inf):
-                    reducedMatrix[i][j] -= min(reducedMatrix[i])
-                    counter += 1
-                    temp += min(reducedMatrix[i])
-
-            if (counter > 0):
-                sumReduce += (temp/counter)
+                sumReduce += minValue
 
         # Scanning columns, reducing column with its minimum element
-        for j in range (len(matrix[0])): # j is the index of column
+        for j in range (len(reducedMatrix[0])): # j is the index of column
             col = [row[j] for row in reducedMatrix]
-            counter = 0
-            temp = 0
-            
-            for i in range (len(matrix[0])): # i is the index of row
-                if (reducedMatrix[i][j] != np.inf):
-                    reducedMatrix[i][j] -= min(col)
-                    counter += 1
-                    temp += min(reducedMatrix[i])
-            
-            if (counter > 0):
-                sumReduce += (temp/counter)
 
-        print('Setelah direduce')
-        for row in reducedMatrix:
-            print(row)
+            # If the minimum is not 0 or infinite, substract all elements in the column
+            # with the minimum value
+            minValue = min(col)
+            if (minValue != 0 and minValue != float('inf')):            
+                for i in range (self.nbVertex): # i is the index of row
+                        reducedMatrix[i][j] -= minValue
 
-        print(sumReduce)
+                sumReduce += minValue
 
         return [reducedMatrix, sumReduce]
 
     def recursiveBnB(self, start, rootNode, route, matrix, cost):
         if (len(route) == self.nbVertex):
-            return route, cost # Route contains index that represent coordinate
+            return route + [start]# Route contains index that represent coordinate
         else:
             # Finding bound value for root node if there's only root node
             if (route == []):
@@ -110,29 +103,21 @@ class Graph():
             
             # Initializing array to contain child nodes
             childNodesMatrix = []
-            childNodesValue = []
+            childNodesCost = []
             notVisitedNodes = list(set([i for i in range(self.nbVertex)]) - set(route))
-            print('Not visited nodes:')
-            print(notVisitedNodes)
 
-            # Making child for rootNode
-            print('\nSebelum direduce:')
-            for row in matrix:
-                print(row)
-
+            # Making child nodes for rootNode
             for node in notVisitedNodes:
                 temp = self.reduceMatrix(matrix, rootNode, node, start)
                 childNodesMatrix += [temp[0]]
-                childNodesValue += [cost + self.distMatrix[rootNode][node] + temp[1]]
+                childNodesCost += [cost + self.distMatrix[rootNode][node] + temp[1]]
 
-            # Getting promising node with minimin cost
-            promisingNode = notVisitedNodes[childNodesValue.index(min(childNodesValue))]
+            # Getting promising node with minimum cost
+            promisingNode = notVisitedNodes[childNodesCost.index(min(childNodesCost))]
             route += [promisingNode]
-            # print('Min value:')
-            # print(min(childNodesValue))
-            matrix = childNodesMatrix[childNodesValue.index(min(childNodesValue))]
+            matrix = childNodesMatrix[childNodesCost.index(min(childNodesCost))]
 
-            return self.recursiveBnB(start, promisingNode, route, matrix, min(childNodesValue))
+            return self.recursiveBnB(start, promisingNode, route, matrix, min(childNodesCost))
 
 # Driver
 C1 = Coordinate(3.5,-0.2,"Google")
@@ -140,19 +125,17 @@ C2 = Coordinate(0,2.3,"Bukalapak")
 C3 = Coordinate(7.9,10,"Gojek")
 C4 = Coordinate(8.1,22,"Tokopedia")
 C5 = Coordinate(0.1,-11,"Amazon")
-# print(C1.eucDistance(C2))
-# print(C1.eucDistance(C3))
 
 distances = [C1, C2, C3, C4, C5]
 G = Graph(distances)
-G.distMatrix = [[np.inf, 10, 17, 0 ,1],
-                [12, np.inf, 11, 2, 0],
-                [0, 3, np.inf, 0, 2],
-                [15, 3, 12, np.inf, 0],
-                [11, 0, 0, 12, np.inf]]
+# G.distMatrix = [[float('inf'), 10, 17, 0 ,1],
+#                 [12, float('inf'), 11, 2, 0],
+#                 [0, 3, float('inf'), 0, 2],
+#                 [15, 3, 12, float('inf'), 0],
+#                 [11, 0, 0, 12, float('inf')]]
 
-
-# print(G.reduceMatrix(0,2))
-print(G.recursiveBnB(0, 0, [], G.distMatrix, 0))
-# for row in G.distMatrix:
-#     print(row)
+I = G.recursiveBnB(0, 0, [], G.distMatrix, 0)
+print(I)
+P = Path(distances, I)
+print(P.countDistance())
+print(P.pathToString())
